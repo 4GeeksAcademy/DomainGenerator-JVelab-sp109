ea1a5ae90dba99aee735f4e264f6e122decfa2b1
Jose Luis Vela <jlvela94@gmail.com>
2025-05-15 22:15:50 +0000

    feat: add inputs to dynamically update arrays and display them
    - Implemented functionality to add new elements to arrays via inputs.
    - Updated UI to display the updated arrays dynamically.

--- src/app.js
+++ src/app.js
@@ -12,13 +12,13 @@ window.onload = function() {
   let noun = ['jogger', 'racoon'];
   let domains = [];
   function generateAllDomains () {
-
     domains = []; // al inicio de generateAllDomains
+
     for (let i = 0; i < pronoun.length; i++) {
       for (let j = 0; j < adj.length; j++) {
         for (let k = 0; k < noun.length; k++) {
           domains.push(pronoun[i] + adj[j] + noun[k] + ".com");
-          console.log(domains);
+          ;
         
         }
       }
@@ -28,13 +28,30 @@ window.onload = function() {
 
   window.agregarValor = function () {
     const inputPronoun = document.getElementById('pronoun');
-    const inputValor = inputPronoun.value.trim();
-    if (inputValor != "") {
-      document.querySelector("#dominios").innerHTML = "";
-      pronoun.push(inputValor);
+    const inputValorpronoun = inputPronoun.value.trim();
+    if (inputValorpronoun != "") {
+      
+      pronoun.push(inputValorpronoun);
       inputPronoun.value = ''; // Limpiar input
       generateAllDomains()
-  
+      document.querySelector("#dominios").innerHTML = domains.join("<br>");    
+    }
+  const inputAdjetive = document.getElementById('adjetive');
+  const inputValoradjetive = inputAdjetive.value.trim();
+  if (inputValoradjetive != "") {
+      
+      adj.push(inputValoradjetive);
+      inputAdjetive.value = ''; // Limpiar input
+      generateAllDomains()
+      document.querySelector("#dominios").innerHTML = domains.join("<br>");    
+    }
+    const inputNoun = document.getElementById('noun');
+    const inputValornoun = inputPronoun.value.trim();
+    if (inputValornoun != "") {
+      
+      noun.push(inputValornoun);
+      inputNoun.value = ''; // Limpiar input
+      generateAllDomains()
       document.querySelector("#dominios").innerHTML = domains.join("<br>");    
     }
 }
